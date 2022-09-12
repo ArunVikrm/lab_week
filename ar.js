fd47eb728df9217b2e12f984b464dc2a79b5a2d0
ArunVikrm <arunvikram07092000@gmail.com>
2022-09-12 09:10:42 +0530

    mobile responsive

--- ar.js
+++ ar.js
@@ -28,7 +28,7 @@ scene.background = camera.backgroundTexture;
 
 // Load a 3D model to place within our group (using ThreeJS's GLTF loader)
 // Pass our loading manager in to ensure the progress bar works correctly
-const comcastSrc = new URL('./assets/logo_6.glb', import.meta.url).href;
+const comcastSrc = new URL('./assets/logo_7.glb', import.meta.url).href;
 const gltfLoader = new THREE.GLTFLoader(manager);
 gltfLoader.load(comcastSrc, (gltf) => {
   // Position the loaded content to overlay user's face
@@ -36,33 +36,8 @@ gltfLoader.load(comcastSrc, (gltf) => {
   gltf.scene.rotation.x = Math.PI/2;
   gltf.scene.scale.set(8, 8, 8);
   scene.add(gltf.scene)
-  console.log('model added')
 }, undefined, () => {
   console.log('An error ocurred loading the GLTF model');
-});
-
-const circleSrc = new URL('./assets/circles_1.glb', import.meta.url).href;
-gltfLoader.load(circleSrc, (gltf) => {
-  // Position the loaded content to overlay user's face
-  gltf.scene.position.set(-0.7, -0.4, -3);
-  gltf.scene.rotation.x = Math.PI/2;
-  gltf.scene.scale.set(0.2, .2, .2);
-  scene.add(gltf.scene)
-  console.log('model added')
-}, undefined, () => {
-  console.log('An error ocurred loading the GLTF model');
-});
-
-const circlepinkeSrc = new URL('./assets/circles_1.glb', import.meta.url).href;
-gltfLoader.load(circlepinkeSrc, (gltf) => {
-    // Position the loaded content to overlay user's face
-    gltf.scene.position.set(0.7, 0.9, -3);
-    gltf.scene.rotation.x = Math.PI/2;
-    gltf.scene.scale.set(0.2, .2, .2);
-    scene.add(gltf.scene)
-    console.log('model added')
-  }, undefined, () => {
-    console.log('An error ocurred loading the GLTF model');
 });
 
 const directionalLight = new THREE.DirectionalLight('white', 0.8);
@@ -77,22 +52,12 @@ const placeButton = document.getElementById('snapshot') || document.createElemen
 
 placeButton.addEventListener('click', () => {
     const canvas = document.querySelector('canvas') || document.createElement('canvas');
-    console.log(isCanvasEmpty(canvas));
     const url = canvas.toDataURL('image/jpeg', 0.8);
     // Take snapshot
     ZapparSharing({
       data: url,
     });
 });
-
-function isCanvasEmpty(cnv) {
-    const blank = document.createElement('canvas');
-
-    blank.width = cnv.width;
-    blank.height = cnv.height;
-
-    return cnv.toDataURL() === blank.toDataURL();
-}
 
 function render() {
     camera.updateFrame(renderer);
